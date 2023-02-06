c848d729d8c3b2e20b1aad3998e5b49fa02411ba
loann rio <rio.loann@gmail.com>
2023-02-06 16:47:04 +0100

    finished part 1

--- day4/aocday4.cpp
+++ day4/aocday4.cpp
@@ -18,14 +18,22 @@ int main() {
     vector<string> lines;
     get_inputs(lines);
     int r = 0;
+    int r2 = 0;
     for (string line : lines) {        
         string fRange = line.substr(0, line.find(','));
         string sRange = line.substr(line.find(',')+1, line.length());
         int range1[2] = {stoi(fRange.substr(0, fRange.find('-'))), stoi(fRange.substr(fRange.find('-')+1, fRange.length()))};
         int range2[2] = {stoi(sRange.substr(0, sRange.find('-'))), stoi(sRange.substr(sRange.find('-')+1, sRange.length()))};
-        if (!((range1[0]>range2[1]) || (range1[1]<range2[0]))) {
+        
+        // part 1 
+        if (((range1[0]>=range2[0])&&(range1[1]<=range2[1]))||((range1[0]<=range2[0])&&(range1[1]>=range2[1]))) {
             r++;
         }
+        
+        // part 2
+        if (!((range1[0]>range2[1]) || (range1[1]<range2[0]))) {
+            r2++;
+        }
     }
-    cout << r;
+    cout << r << " " << r2;
 }
