65903f158a73f6b366ef28212f2beeda21983a11
loann rio <rio.loann@gmail.com>
2023-02-09 23:33:09 +0100

    worked on part 1

--- day9/aocday9.cpp
+++ day9/aocday9.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <algorithm>
+#include <cmath>
 #include <vector>
 #include <list>
 #include <bits/stdc++.h>
@@ -16,20 +17,48 @@ void get_inputs(list<string>& inputs) {
     IN.close();
 }
 
-// void appendToVect(vector<tuple<int, int>> & visited, tuple<int, int> pos) {
-//     if (find_if(visited.begin(), visited.end(), pos) == visited.end()) {
-//         visited.push_back(pos);
-//     }
-// }
+template <typename T> int sgn(T val) {
+    return (T(0) < val) - (val < T(0));
+}
+
+void appendToVect(vector<tuple<int, int>> & visited, tuple<int, int> pos) {
+    // append the position to the vector if not already in
+    for (tuple<int, int> pastpos : visited) {
+        if (pastpos == pos) {
+            return;
+        }
+    }
+    visited.push_back(pos);
+}
+
+
+int getDir(char dir) {
+    switch (dir)
+    {
+    case 'R':
+        return 1;
+    case 'L':
+        return 3;
+    case 'U':
+        return 0;
+    case 'D':
+        return 2;
+    }   
+}
 
 int main() {
+    // get inputs from file
     list<string> inputs;
     get_inputs(inputs);
 
+    // directions
     int xdir[4] = {0, 1, 0, -1};
     int ydir[4] = {1, 0, -1, 0};
 
-    int8_t dir = 0;
+    tuple<int, int> 
+
+    // head direction
+    int dir = 0;
 
     int posXHead = 0;
     int posYHead = 0;
@@ -37,28 +66,36 @@ int main() {
     int posXtail = 0;
     int posYtail = 0;
 
-    //vector<tuple<int, int>> visited;
+    // vector of all visited position {set}
+    vector<tuple<int, int>> visited;
 
     for (string row : inputs) {
-        cout << row << '\n';
+        dir = getDir(row[0]); // get direction from input
 
-        if (row[0] == 'R') {
-            dir = (dir + 1)%4;
-        } else { 
-            dir = (dir - 1)%4; 
-        }
+        for (int k=0;k<stoi(row.substr(2)); k++) { // for all repetion of inputs action
 
-        cout << dir << "\n";
-
-        for (int k=0;k<stoi(row.substr(2)); k++) {
-            cout << xdir[dir] << " " << ydir[dir] << "\n";
+            // update head pos
             posXHead += xdir[dir];
             posYHead += ydir[dir];
-            cout << posXHead << " " << posYHead << "\n";
+
+            double dist = sqrt((posXHead-posXtail)*(posXHead-posXtail)+ (posYHead-posYtail)*(posYHead-posYtail));
+
+            if (dist == 2) {
+                posXtail += (int) (posXHead-posXtail)/2;
+                posYtail += (int) (posYHead-posYtail)/2;
+                
+            } else if (dist > 2) {
+                posXtail += sgn(posXHead-posXtail);
+                posYtail += sgn(posYHead-posYtail);
+            }
+
+            tuple<int, int> pos = {posXtail, posYtail};
+            appendToVect(visited, pos);
 
         }
         
     }
-    
+
+    cout << visited.size();
 
 }
