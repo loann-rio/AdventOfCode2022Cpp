13c826251538e2c6960c6d45d6ddc6142e14ecd4
loann rio <rio.loann@gmail.com>
2023-02-10 11:17:28 +0100

    inproved code + solve part 2

--- day9/aocday9.cpp
+++ day9/aocday9.cpp
@@ -17,6 +17,22 @@ void get_inputs(list<string>& inputs) {
     IN.close();
 }
 
+tuple<int, int> addTuple(tuple<int, int> a, tuple<int, int> b) {
+    return {get<0>(a)+get<0>(b), get<1>(a)+get<1>(b)};    
+}
+
+tuple<int, int> subTuple(tuple<int, int> a, tuple<int, int> b) {
+    return {get<0>(a)-get<0>(b), get<1>(a)-get<1>(b)};    
+}
+
+tuple<int, int> divTuple(tuple<int, int> a, float b) {
+    return {get<0>(a)/b, get<1>(a)/b};    
+}
+
+double sqr(double a) {
+    return a*a;
+}
+
 template <typename T> int sgn(T val) {
     return (T(0) < val) - (val < T(0));
 }
@@ -27,18 +43,33 @@ void appendToVect(vector<tuple<int, int>> & visible, tuple<int, int> pos) {
     }
 }
 
-int getDir(char dir) {
+tuple<int, int> getDir(char dir) {
     switch (dir)
     {
     case 'R':
-        return 1;
+        return {1, 0};
     case 'L':
-        return 3;
+        return {-1, 0};
     case 'U':
-        return 0;
+        return {0, 1};
     case 'D':
-        return 2;
+        return {0, -1};
     }   
+}
+
+tuple<int, int> move(tuple<int, int> posHead, tuple<int, int> postail) {
+    tuple<int, int> diff = subTuple(posHead, postail);
+
+    double dist = sqrt(sqr(get<0>(diff)) + sqr(get<1>(diff)));
+
+    if (dist == 2) {
+        postail = addTuple(divTuple(diff, 2), postail);
+        
+    } else if (dist > 2) {
+        get<0>(postail) += sgn(get<0>(diff));
+        get<1>(postail) += sgn(get<1>(diff));
+    }
+    return postail;
 }
 
 int main() {
@@ -46,48 +77,27 @@ int main() {
     list<string> inputs;
     get_inputs(inputs);
 
-    // directions
-    int xdir[4] = {0, 1, 0, -1};
-    int ydir[4] = {1, 0, -1, 0};
-
-    // head direction
-    int dir = 0;
-
-    int posXHead = 0;
-    int posYHead = 0;
-
-    int posXtail = 0;
-    int posYtail = 0;
+    vector<tuple<int, int>> poss(10);
 
     // vector of all visited position {set}
     vector<tuple<int, int>> visited;
 
     for (string row : inputs) {
-        dir = getDir(row[0]); // get direction from input
+        tuple<int, int> dir = getDir(row[0]); // get direction from input
 
         for (int k=0;k<stoi(row.substr(2)); k++) { // for all repetion of inputs action
 
             // update head pos
-            posXHead += xdir[dir];
-            posYHead += ydir[dir];
+            poss[0] = addTuple(poss[0], dir);
 
-            double dist = sqrt((posXHead-posXtail)*(posXHead-posXtail)+ (posYHead-posYtail)*(posYHead-posYtail));
-
-            if (dist == 2) {
-                posXtail += (int) (posXHead-posXtail)/2;
-                posYtail += (int) (posYHead-posYtail)/2;
-                
-            } else if (dist > 2) {
-                posXtail += sgn(posXHead-posXtail);
-                posYtail += sgn(posYHead-posYtail);
+            // update tail
+            for (int i=0; i< poss.size()-1; i++) {
+                poss[i+1] = move(poss[i], poss[i+1]);
             }
-
-            appendToVect(visited, {posXtail, posYtail});
-
+            
+            // save the pos of the last knot
+            appendToVect(visited, poss.back());
         }
-        
     }
-
     cout << visited.size();
-
 }
