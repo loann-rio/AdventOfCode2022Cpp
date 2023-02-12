4cf609513a0bd1a57afc80420137c217905692f2
rio.loann <rio.loann@gmail.com>
2023-02-12 22:49:30 +0100

    I don t understand regex

--- day11/aocday11.cpp
+++ day11/aocday11.cpp
@@ -8,11 +8,11 @@
 
 using namespace std;
 
-void get_inputs(vector<string>& inputs, string file) {
+void get_inputs(string& inputs, string file) {
     ifstream IN(file);
     string text;
     while (getline(IN, text)) {
-        inputs.push_back(text);
+        inputs += '\n' + text;
     }
     IN.close();
 }
@@ -24,22 +24,37 @@ struct monkey
     vector<int> result;
 };
 
-vector<monkey> process_inputs(vector<string> inputs) {
+void process_inputs(string inputs) {
     vector<monkey> monkeys;
-    for (int i=0; i< inputs.size(); i+= 7) {
-        string itemsRow = inputs[i+1];
-        itemsRow = itemsRow.substr(18);
-        for (int j=0; j<itemsRow.size(); j+=4) {
+    // use of regex
+    // first line:
+    // Starting\sitems:\s([1-9]{2},\s)*
 
+    // sec line: Operation:\snew\s=\sold\s(\+|\*)\s(.*)
+    // Test:\sdivisible\sby\s([1-9]+)
+    // If\s((\btrue\b)|(\bfalse\b)):\sthrow\sto\smonkey\s([0-9])
+
+
+    /*Starting\sitems:\s(([0-9]{2},\s)*)\n\s\sOperation:\snew\s=\sold\s(\+|\*)\s(.*)\n\s\sTest:\sdivisible\sby\s([1-9]+)\n\s\s\s\sIf\strue:\sthrow\sto\smonkey\s([0-9])\n\s\s\s\sIf\sfalse:\sthrow\sto\smonkey\s([0-9])*/
+
+    regex rgx ("Starting\\sitems:\\s(([0-9]{2},\\s)*)\n\\s\\sOperation:\\snew\\s=\\sold\\s");
+    smatch matches;
+
+    if(regex_search(inputs, matches, rgx)) {
+        cout << "Match found\n";
+
+        for (size_t i = 0; i < matches.size(); ++i) {
+            cout << i << ": '" << matches[i].str() << "'\n";
         }
-
-
+    } else {
+        cout << "Match not found\n";
     }
 }
 
 
 int main() {
-    vector<string> inputs;
+    string inputs;
     get_inputs(inputs, "inputs.txt");
+    process_inputs(inputs);
     
 }
